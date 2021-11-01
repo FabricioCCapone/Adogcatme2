7ddf033247ec45bef2bec8d2fb4cd13cc6437ffb
fabricio.capone <fabricapone12@gmail.com>
2021-11-01 20:47:57 -0300

    Filtros

--- src/main/java/Adogcatme/Proyecto/Repositorios/MascotaRepositorio.java
+++ src/main/java/Adogcatme/Proyecto/Repositorios/MascotaRepositorio.java
@@ -6,6 +6,7 @@
 package Adogcatme.Proyecto.Repositorios;
 
 import Adogcatme.Proyecto.entidades.Mascota;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,6 +17,15 @@ import org.springframework.data.repository.query.Param;
  */
 public interface MascotaRepositorio extends JpaRepository<Mascota, String>{
     
-    @Query("SELECT m FROM Mascota m WHERE m.ubicacion = :")
-    public Mascota findBy(@Param("ubica") String ubicacion); 
+    @Query("SELECT m FROM Mascota m JOIN Dueno d WHERE d.id = m.dueno_id AND (SELECT * FROM Dueno dd, Adoptante a WHERE a.ciudad AND dd.ciudad = :ciudad)")
+    public List<Mascota> findByUbicacion(@Param("ciudad") String ciudad); 
+
+    @Query("SELECT m FROM Mascota m WHERE tipo = :tipo)")
+    public List<Mascota> findByTipo(@Param("tipo") String tipo); 
+ 
+    @Query("SELECT m FROM Mascota m WHERE sexo = :sexo)")
+    public List<Mascota> findBySexo(@Param("sexo") String sexo); 
+
+    @Query("SELECT m FROM Mascota m WHERE castrado = :castrado)")
+    public List<Mascota> findByCastrado(@Param("castrado") Integer castrado); 
 }
