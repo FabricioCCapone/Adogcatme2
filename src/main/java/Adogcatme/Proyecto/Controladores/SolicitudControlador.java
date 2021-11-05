a6528db999f05b3fe7a675918f16632e564a3789
FabricioCCapone <91097649+FabricioCCapone@users.noreply.github.com>
2021-11-04 21:21:58 -0300

    Primeros avances en conectando con front

--- src/main/java/Adogcatme/Proyecto/Controladores/SolicitudControlador.java
+++ src/main/java/Adogcatme/Proyecto/Controladores/SolicitudControlador.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Extre
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/solicitud")
 public class SolicitudControlador {
     
     @Autowired
